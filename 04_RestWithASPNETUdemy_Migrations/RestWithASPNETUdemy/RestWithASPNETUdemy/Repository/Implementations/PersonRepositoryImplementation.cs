b6d157aa3cbf7cd01d70578ba662ca544404901a
Marcus Bastos <mvcbastos@gmail.com>
2021-06-24 21:38:13 -0300

    Migrations and datasets implemented

---
+++ 04_RestWithASPNETUdemy_Migrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,87 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySQLContext _context;
+
+        public PersonRepositoryImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+
+        List<Person> IPersonRepository.FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+
+        Person IPersonRepository.FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+
+        Person IPersonRepository.Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        void IPersonRepository.Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        Person IPersonRepository.Update(Person person)
+        {
+            if (!Exists(person.Id)) return null;
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
---
+++ 04_RestWithASPNETUdemy_Migrations/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_table_Person.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Sena', 'Male'),
+	(2, 'Anchiano - Itália', 'Leonardo', 'Male', 'da Vinci'),
+	(3, 'Porbandar - Índia', 'Mahatma', 'Male', 'Gandhi'),
+	(4, 'Kentucky - USA', 'Mohamed', 'Male', 'Ali'),
+	(5, 'Mvezo - África do Sul', 'Nelson', 'Male', 'Mandela');
